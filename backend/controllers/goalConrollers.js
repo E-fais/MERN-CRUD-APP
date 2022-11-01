eb56945ad7fe02a195fdbc26d6b64f1e28e5aee1
fais edathil <faiesedathil@gmail.com>
2022-11-01 21:53:50 +0300

    complete mongo crud

--- backend/controllers/goalConrollers.js
+++ backend/controllers/goalConrollers.js
@@ -1,23 +1,41 @@
 const asyncHandler=require('express-async-handler')
+const Goals=require('../model/model')
 
 const getGoals=asyncHandler( async (req,res)=>{
-    res.status(200).json({message:'goals from controllers'})
+    const goal= await Goals.find()
+    res.status(200).json(goal)
 })
 
 const updateGoal=asyncHandler( async (req,res)=>{
-    res.status(200).json({message:`update from controller ${req.params.id}`})
+    const goal=await Goals.findById(req.params.id)
+    if(!goal){
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+    const updatedGoal=await Goals.findByIdAndUpdate(req.params.id,req.body,
+        {new:true})
+    res.status(200).json(updatedGoal)
 })
 const postGoal=asyncHandler( async (req,res)=>{
    if(!req.body.text){
     res.status(400)
     throw new Error('please add text');
    }
-    res.status(200).json({message:'create a new goal'})
+   const goals=await Goals.create(
+    {
+        text:req.body.text
+    }
+   )
+    res.status(200).json(goals)
 })
 const deleteGoal=asyncHandler( async (req,res)=>{
-    res.status(200).json({
-        message:`delete goals ${req.params.id}`
-    })
+    const goal=await Goals.findById(req.params.id)
+    if(!goal){
+        res.status(400)
+        throw new Error
+    }
+ await goal.remove()
+    res.status(200).json(req.params.id)
 })
 module.exports={
     getGoals,
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 const express =require ('express')
+const colors=require('colors')
 const {errorHandler}=require('./middleWares/middleWare')
+const connectDB=require('./MongoDB/config')
 const  dotenv=require('dotenv').config()
 const port =process.env.PORT ||8000
 
 const app= express()
 app.use(express.json())
+connectDB()
 app.use(errorHandler)
 app.use(express.urlencoded({extended:false}))
 app.use('/api/goals',require('./Routes/goalRoutes'))
