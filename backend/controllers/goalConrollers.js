5f7833e24866f2a3971fb3d10b105f050a2f2c6b
fais edathil <faiesedathil@gmail.com>
2022-11-01 20:02:27 +0300

    controllers and routes setup

--- backend/controllers/goalConrollers.js
+++ backend/controllers/goalConrollers.js
@@ -1,17 +1,24 @@
-const getGoals= (req,res)=>{
+const asyncHandler=require('express-async-handler')
+
+const getGoals=asyncHandler( async (req,res)=>{
     res.status(200).json({message:'goals from controllers'})
-}
-const updateGoal=(req,res)=>{
+})
+
+const updateGoal=asyncHandler( async (req,res)=>{
     res.status(200).json({message:`update from controller ${req.params.id}`})
-}
-const postGoal=(req,res)=>{
+})
+const postGoal=asyncHandler( async (req,res)=>{
+   if(!req.body.text){
+    res.status(400)
+    throw new Error('please add text');
+   }
     res.status(200).json({message:'create a new goal'})
-}
-const deleteGoal=(req,res)=>{
+})
+const deleteGoal=asyncHandler( async (req,res)=>{
     res.status(200).json({
         message:`delete goals ${req.params.id}`
     })
-}
+})
 module.exports={
     getGoals,
     postGoal,
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const express =require ('express')
+const {errorHandler}=require('./middleWares/middleWare')
 const  dotenv=require('dotenv').config()
 const port =process.env.PORT ||8000
 
 const app= express()
+app.use(express.json())
+app.use(errorHandler)
+app.use(express.urlencoded({extended:false}))
 app.use('/api/goals',require('./Routes/goalRoutes'))
 app.listen(port,console.log('server connected'))
